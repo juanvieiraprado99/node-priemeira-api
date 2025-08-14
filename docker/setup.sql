d3e28d74adb9ca05d568d052493265cdeef87b85
juanvieiraprado99 <juanprado99@gmail.com>
2025-08-14 13:24:29 -0300

    feat: add husky for pre-commit hooks

--- docker/setup.sql
+++ docker/setup.sql
@@ -1,6 +1,3 @@
--- Script de inicialização do banco de dados
--- Este arquivo é executado automaticamente quando o container é criado
-
 -- Criar banco de teste se não existir
 SELECT 'CREATE DATABASE desafio_test'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'desafio_test')\gexec
